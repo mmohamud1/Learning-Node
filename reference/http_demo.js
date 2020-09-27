69a784b33f2ff14c98ca7c9e0615820f4ee8a470
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-09-28 00:11:31 +0200

    Commit

--- reference/http_demo.js
+++ reference/http_demo.js
@@ -4,4 +4,5 @@ const http = require('http');
 http.createServer((req, res) => {
     // Write response
     res.write("Hello World");
-});
+    res.end()
+}).listen(5000, () => console.log('Server running...'));
